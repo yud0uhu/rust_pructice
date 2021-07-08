7191f5984fbb28b532ea08b8b7b3395e020a080b
root <b219080@photon.chitose.ac.jp>
2021-07-08 10:14:21 +0900

    first init

---
+++ hello_cargo/variables/target/debug/deps/variables-3b736149aabe80bf.d
@@ -0,0 +1,5 @@
+/hello_rust/hello_cargo/variables/target/debug/deps/variables-3b736149aabe80bf: src/main.rs
+
+/hello_rust/hello_cargo/variables/target/debug/deps/variables-3b736149aabe80bf.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/hello_rust/hello_cargo/variables/target/debug/variables: /hello_rust/hello_cargo/variables/src/main.rs
