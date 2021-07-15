bf63406654414d944962f165f161a7270a305bec
root <b219080@photon.chitose.ac.jp>
2021-07-15 22:48:06 +0900

    mv sosuu_1.rs

---
+++ hello_cargo/src/algo/sosuu_1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    sosuu();
+}
+
+fn main() {
+    let mut _j = 0;
+    let mut _i = 0;
+    for _i in 2..101 {
+        let mut _h = _i/2;
+        for _j in 2..(_h+1) {
+            if _i%_j == 0 {
+                false;
+                break;
+            }
+            if true {
+                println!("{}",_i);
+                }
+            }
+    }
+}
